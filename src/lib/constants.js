5f3c8f461d5a7b189fe59b1d62b91f7abde318c6
suresh kallem <suresh.k@cloudtaru.com>
2021-09-20 01:32:33 +0530

    Adding .env file with DB connection string as a env variable

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,7 @@
 const ROUTE_VERSION = 'v1';
 const ROUTE_API = 'api';
 const ROUTE_AUTH = 'auth';
-const DB_CONNECTION_STRING = 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getircase-study?retryWrites=true';
+const DB_CONNECTION_STRING =process.env.DB_CONNECTION_STRING || 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getircase-study?retryWrites=true';
 const DB_NAME = 'getir-case-study';
 const COLLECTION_NAME = 'records';
 
